cbc0085d9d3fbdcb6f19e1060e5b04aaca2494c5
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2022-10-06 16:37:07 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -84,11 +84,13 @@ $(function () {
             myStream.getAudioTracks()[0].enabled = false
             html = `<i class = "fa fa-microhone-slash"></i>`
             $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html);
         }
         else(enabled){
             myStream.getAudioTracks()[0].enabled = true
             html = `<i class = "fa fa-microhone"></i>`
             $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html);
         }
     })
 
